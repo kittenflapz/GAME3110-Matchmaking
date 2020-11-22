6253efdd49e8c0878f856d375fecf21c9d4b084e
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-11-22 16:12:20 -0500

    restructuring

--- gameSim.py
+++ gameSim.py
@@ -1,16 +1,44 @@
-import mm
-import requests
-import random
 import logging
+import sys
 import datetime
+import random
+import socket
+import time
+import threading
+from datetime import datetime
+import json
+import requests
+
+serverIP = "3.137.149.42"
+serverPort = 12345
+logging.basicConfig(filename='assignment3log.log', level=logging.INFO)
+
+numGames = input("Enter number of games you'd like to simulate: ")
+
+count = 0
+while count < numGames:
+    logging.info('Game ID: ' + str(count) + ' requested at ' + datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
+    runGame()
+    count += 1
+
 
 def runGame():
 
     # Make list of possible player IDs (for testing)
     idList = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011']
 
-    # Make a room requested by a random one of the players in our database
-    playersInGame = mm.makeRoomOfThree(random.choice(idList))
+
+    # Send a random id of a player to the matchmaking server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(serverIP, serverPort)
+        id = json.dumps(random.choice(idList))
+        print('Send', id)
+        sock.sendall(id)
+        data = sock.recv(1024)
+
+
+    # Get a list of players in the game
+    playersInGame = json.loads(data)
 
     competingIDs = []
 
@@ -37,7 +65,6 @@ def runGame():
         
         # Log skill level of winner after the game
         response = requests.get('https://iu546cqfr3.execute-api.us-east-2.amazonaws.com/default/getAllPlayers')
-        responseJson = response.json()
         players = response.json()['Items']
         for player in players:
             if player['playerID'] == winner:
@@ -50,7 +77,6 @@ def runGame():
             requests.get('https://bg5i4i7ta0.execute-api.us-east-2.amazonaws.com/default/updatePlayerScore', params={'playerID': loser, 'lost': 'lost'})
             # Log skill level of losers after the game
             response = requests.get('https://iu546cqfr3.execute-api.us-east-2.amazonaws.com/default/getAllPlayers')
-            responseJson = response.json()
             players = response.json()['Items']
             for player in players:
                 if player['playerID'] == loser:
--- mm.py
+++ mm.py
@@ -1,76 +1,101 @@
 import requests
 import json
 import sys
-import logging
 import datetime
+import random
+import time
+import socket
+from _thread import *
+import threading
 from operator import itemgetter
+import logging
 
-def makeRoomOfThree(IDofPlayerRequestingMatch):
-    # Get the player database
-    response = requests.get('https://iu546cqfr3.execute-api.us-east-2.amazonaws.com/default/getAllPlayers')
-
-    # Turn it into a json object
-    responseJson = response.json()
-
-    # Get just the player list from the database (the items)
-    players = response.json()['Items']
-
-    # Get the first argument we passed in and hope it is a valid ID
-    # TO DO: Validate the argument
-    #testID = str(sys.argv[1])
-
-    tolerance = 20
-    playerRequestingMatch = None
-
-    # Print the player's info whose ID was passed in
-    # While we are here, set any players who have played less than 3 games' win percentage to 50
-    print('Player searching for match: ')
-    for player in players:
-        if player['playerID'] == IDofPlayerRequestingMatch:
-            print(player)
-            playerRequestingMatch = player
-        if player['totalGames'] < 3:
-            player['winPercentage'] = 50.0
-
-    # Print everyone else's info
-    print('All available players: ')
-    for player in players:
-        if player != playerRequestingMatch:
-            print(player)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+logging.basicConfig(filename='matchmakingServer.log', level=logging.INFO)
 
 
-    potentialPlayers = []
-    # Make a list of available players in tolerance
-    for player in players:
-        if abs(player['winPercentage']-playerRequestingMatch['winPercentage']) < tolerance and player != playerRequestingMatch:
-            potentialPlayers.append(player)
+def makeRoomOfThree():
+    while True:
+        sock.listen()
+        conn, addr = sock.accept()
 
-    # Sort the list by win percentage
-    sortedPotentialPlayers = sorted(potentialPlayers, key=itemgetter('winPercentage'))
+        with conn:
+            IDofPlayerRequestingMatch = conn.recv(1024)
+            IDofPlayerRequestingMatch = json.loads(IDofPlayerRequestingMatch)
+            if IDofPlayerRequestingMatch != "":
+                # Get the player database
+                response = requests.get(
+                    'https://iu546cqfr3.execute-api.us-east-2.amazonaws.com/default/getAllPlayers')
 
-    # Make a list of tuples of player(dict) and float (distance from player requesting game win% to player available win%)
-    doubleSortedPotentialPlayers = [(player, abs(player['winPercentage']-playerRequestingMatch['winPercentage'])) for player in sortedPotentialPlayers] 
+                # Get just the player list from the database (the items)
+                players = response.json()['Items']
 
-    # Sort the list of tuples by their dist
-    doubleSortedPotentialPlayers.sort(key=itemgetter(1))
+                tolerance = 20
+                playerRequestingMatch = None
 
-    # Turn the list of tuples back into a list of just dicts
-    finalSortedPlayers = [seq[0] for seq in doubleSortedPotentialPlayers]
+                # Print the player's info whose ID was passed in
+                # While we are here, set any players who have played less than 3 games' win percentage to 50
+                print('Player searching for match: ')
+                for player in players:
+                    if player['playerID'] == IDofPlayerRequestingMatch:
+                        print(player)
+                        playerRequestingMatch = player
+                    if player['totalGames'] < 3:
+                        player['winPercentage'] = 50.0
 
-    print('Available players within tolerance of ' + str(tolerance) + ': ')
-    for player in finalSortedPlayers:
-        print(player)
+                # Print everyone else's info
+                print('All available players: ')
+                for player in players:
+                    if player != playerRequestingMatch:
+                        print(player)
 
-    # Remove items from end of list until we have the 2 players closest in win % to the player requesting the game
-    while len(finalSortedPlayers) > 2:
-        finalSortedPlayers.pop()
-    
-    # Finally, add the player requesting the game back to the list
-    finalSortedPlayers.append(playerRequestingMatch)
+                potentialPlayers = []
+                # Make a list of available players in tolerance
+                for player in players:
+                    if abs(player['winPercentage']-playerRequestingMatch['winPercentage']) < tolerance and player != playerRequestingMatch:
+                        potentialPlayers.append(player)
 
-    for player in finalSortedPlayers:
-        logging.info('Following player connected at ' + datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
-        logging.info(player)
+                # Sort the list by win percentage
+                sortedPotentialPlayers = sorted(
+                    potentialPlayers, key=itemgetter('winPercentage'))
 
-    # Return the final list
-    return finalSortedPlayers
+                # Make a list of tuples of player(dict) and float (distance from player requesting game win% to player available win%)
+                doubleSortedPotentialPlayers = [(player, abs(
+                    player['winPercentage']-playerRequestingMatch['winPercentage'])) for player in sortedPotentialPlayers]
+
+                # Sort the list of tuples by their dist
+                doubleSortedPotentialPlayers.sort(key=itemgetter(1))
+
+                # Turn the list of tuples back into a list of just dicts
+                finalSortedPlayers = [seq[0] for seq in doubleSortedPotentialPlayers]
+
+                print('Available players within tolerance of ' + str(tolerance) + ': ')
+                for player in finalSortedPlayers:
+                    print(player)
+
+                # Remove items from end of list until we have the 2 players closest in win % to the player requesting the game
+                while len(finalSortedPlayers) > 2:
+                    finalSortedPlayers.pop()
+
+                # Finally, add the player requesting the game back to the list
+                finalSortedPlayers.append(playerRequestingMatch)
+
+                for player in finalSortedPlayers:
+                    logging.info('Following player connected at ' +
+                                datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
+                    logging.info(player)
+
+                # Return the final list
+                response = json.dumps(finalSortedPlayers)
+                conn.sendall(response)
+
+
+
+ip = ''  
+port = 12345        
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((ip, port))
+    start_new_thread(makeRoomOfThree, (s,))
+    while True:
+        time.sleep(1)
